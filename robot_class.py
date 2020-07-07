1255f94e109d990d80bd29772bee8fea07b97b6c
ava6969 <ava6969@rit.edu>
2020-07-07 17:45:23 -0400

    singularity matrix

--- robot_class.py
+++ robot_class.py
@@ -76,19 +76,24 @@ class robot:
             This function should account for measurement_noise and measurement_range.
             One item in the returned list should be in the form: [landmark_index, dx, dy].
             '''
-           
         measurements = []
-        
+           
         ## TODO: iterate through all of the landmarks in a world
-        
+        for i, landmark in enumerate(self.landmarks):
         ## TODO: For each landmark
         ## 1. compute dx and dy, the distances between the robot and the landmark
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
+            dx = landmark[0] - self.x + self.rand() * self.measurement_noise
+            dy = landmark[1] - self.y + self.rand() * self.measurement_noise
+            if self.measurement_range == -1 \
+            or (dx <= self.measurement_range and dy <= self.measurement_range):
+                measurements.append([i, dx, dy])
         
         ## TODO: return the final, complete list of measurements
         return measurements
